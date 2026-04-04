d9f64e192233e3866fe77cb77dc0a5ae57c95a8e
Sludge <sludge.phd+git@gmail.com>
2026-04-04 03:03:57 +0200

    init

---
+++ build.rs
@@ -0,0 +1,27 @@
+use std::{ffi::OsStr, fs, io, path::PathBuf};
+
+fn find_sources(basedir: &str) -> io::Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
+    for res in fs::read_dir(basedir)? {
+        let ent = res?;
+        let path = ent.path();
+        if ent.file_type()?.is_file() && path.extension() == Some(OsStr::new("c")) {
+            files.push(path);
+        }
+    }
+    Ok(files)
+}
+
+fn main() -> io::Result<()> {
+    let mylib_src = find_sources("TestU01-2009/mylib")?;
+    let probdist_src = find_sources("TestU01-2009/probdist")?;
+    let testu01_src = find_sources("TestU01-2009/testu01")?;
+
+    cc::Build::new()
+        .include("TestU01-2009")
+        .files(mylib_src)
+        .files(probdist_src)
+        .files(testu01_src)
+        .compile("testu01");
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+#[link(name = "testu01")]
+unsafe extern "C" {}
+
+fn main() {
+    println!("Hello, world!");
+}
